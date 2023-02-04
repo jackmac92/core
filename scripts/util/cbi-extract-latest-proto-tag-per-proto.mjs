65c9827ca2f4902d50660fbf43d55a4b554adf40
Jack McCown <jackmac79@gmail.com>
2023-02-03 22:57:41 -0500

    script to regenerate all PaP for protos repo

---
+++ scripts/util/cbi-extract-latest-proto-tag-per-proto.mjs
@@ -0,0 +1,31 @@
+#!/usr/bin/env zx
+import _ from "lodash";
+
+const allTags = await $`git tag --list`.quiet();
+
+const tags = allTags
+  .toString()
+  .split("\n")
+  .filter((t) => t.includes("/"))
+  .filter((t) => !t.includes("beta"))
+  .filter((t) => !t.endsWith("-dev"))
+  .filter((t) => !t.endsWith("-stg"))
+  .filter((t) => {
+    const [_, version] = t.split("/");
+    return version.split(".").length === 3;
+  });
+
+const x = _.groupBy(tags, (t) => t.split("/")[0]);
+Object.keys(x).forEach((k) => {
+  x[k] = _.max(x[k], (t) => t.split("/")[1]).split("/")[1];
+});
+
+const latestProtos = Object.keys(x).map((k) => `${k}/${x[k]}`);
+
+latestProtos.slice(0, 2).map((t) => {
+  $`lab ci create ${t}`;
+});
+
+// Local Variables:
+// mode: js2
+// End:
